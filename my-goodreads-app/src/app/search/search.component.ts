309957945a29329de302a9ba6c21a5ba4e205964
Celia Martin <cmartinperez@mimecast.com>
2019-07-22 13:45:51 +0100

    added mapping of request to Book model

--- my-goodreads-app/src/app/search/search.component.ts
+++ my-goodreads-app/src/app/search/search.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Subject, fromEvent } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { Book } from '../shelf/book/book.model';
 
 @Component({
   selector: 'app-search',
@@ -13,7 +15,7 @@ export class SearchComponent implements OnInit {
   searchUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
   maxResults = '&maxResults=10';
 
-  books: any[];
+  books: Book[];
   searchField: FormControl = new FormControl();
 
   constructor(private http: HttpClient) {
@@ -21,12 +23,21 @@ export class SearchComponent implements OnInit {
 
   ngOnInit() {
     this.searchField.valueChanges
+      .pipe(debounceTime(300), distinctUntilChanged())
       .subscribe(searchTerm => this.search(searchTerm)
-        .subscribe(results => this.books = results.items));
+        .subscribe(results => this.books = results));
   }
 
   search(term) {
-    return this.http.get(this.searchUrl + term + this.maxResults);
+    return this.http.get(this.searchUrl + term + this.maxResults)
+      .pipe(map((data: any[]) => data.items.map((item: any) => new Book(
+        item.id,
+        item.volumeInfo.title,
+        item.volumeInfo.authors[0],
+        item.volumeInfo.description,
+        item.volumeInfo.pageCount,
+        item.volumeInfo.imageLinks.thumbnail,
+      ))));
   }
 
 }
